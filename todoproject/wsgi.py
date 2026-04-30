1bb48acd0e7962b2fde2eeb37dd5d8e1a8331721
vincent mugondora <vinmugondora@gmail.com>
2026-04-30 13:52:18 +0200

    Create admin user from environment

--- todoproject/wsgi.py
+++ todoproject/wsgi.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 
 import os
 
+from django.contrib.auth import get_user_model
 from django.core.management import call_command
 from django.core.wsgi import get_wsgi_application
 
@@ -18,3 +19,20 @@ application = get_wsgi_application()
 
 if os.environ.get('RUN_MIGRATIONS_ON_STARTUP', 'True') == 'True':
     call_command('migrate', interactive=False, verbosity=1)
+
+admin_username = os.environ.get('ADMIN_USERNAME')
+admin_password = os.environ.get('ADMIN_PASSWORD')
+admin_email = os.environ.get('ADMIN_EMAIL', '')
+
+if admin_username and admin_password:
+    User = get_user_model()
+    user, created = User.objects.get_or_create(
+        username=admin_username,
+        defaults={'email': admin_email, 'is_staff': True, 'is_superuser': True},
+    )
+    if created or not user.check_password(admin_password):
+        user.email = admin_email
+        user.is_staff = True
+        user.is_superuser = True
+        user.set_password(admin_password)
+        user.save()
