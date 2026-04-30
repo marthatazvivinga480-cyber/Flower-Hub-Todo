c2e53b59c39866dddf802d3b6fad9dd4f99d2bbc
vincent mugondora <vinmugondora@gmail.com>
2026-04-30 12:50:56 +0200

    updating

--- todoproject/urls.py
+++ todoproject/urls.py
@@ -21,6 +21,14 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('todos.urls')),
-    path('login/', auth_views.LoginView.as_view(template_name='todos/login.html'), name='login'),
+    path(
+        'login/',
+        auth_views.LoginView.as_view(
+            template_name='todos/login.html',
+            next_page='todo_list',
+            redirect_authenticated_user=True,
+        ),
+        name='login',
+    ),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
